2f5035de2f5c1986c0f0a03309c7f65f8e0297e6
OmkarRajeshKokane <75972077+OmkarRajeshKokane@users.noreply.github.com>
2021-01-04 16:09:16 +0530

    Update javaApplication.java

--- javaApplication.java
+++ javaApplication.java
@@ -398,7 +398,7 @@ public static void Emptycart(String username,Statement stmt) throws SQLException
             choice = Integer.parseInt(ds.readLine());
             try{
             Class.forName("com.mysql.jdbc.Driver");
-            Connection con=DriverManager.getConnection("jdbc:mysql://sql12.freesqldatabase.com:3306/sql12371159","sql12371159","2hq7cvQ8S1");
+            Connection con=DriverManager.getConnection("Your_database_URL","Username_for_database","Password");
             Statement stmt=con.createStatement();
             
             switch(choice){
